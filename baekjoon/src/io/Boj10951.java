190a018f37eb7bc882097222b4240ac76d3cb0f0
CH <hbac11@naver.com>
2023-01-13 23:33:57 +0900

    Java 백준 입출력1

---
+++ baekjoon/src/io/Boj10951.java
@@ -0,0 +1,10 @@
+package io;
+
+import java.io.*;
+
+public class Boj10951 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
