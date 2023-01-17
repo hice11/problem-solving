d97203214229692829b566358ca59b1731ad90cc
CH <hbac11@naver.com>
2023-01-18 05:28:49 +0900

    Java 백준 입출력2

--- baekjoon/src/io/Boj10952.java
+++ baekjoon/src/io/Boj10952.java
@@ -1,19 +1,21 @@
 package io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Boj10951 {
+public class Boj10952 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         String str;
-        while ((str = br.readLine()) != null) {
+        while (!(str = bf.readLine()).equals("0 0")) {
             st = new StringTokenizer(str, " ");
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
-            sb.append(A + B).append("\n");
+            sb.append(A + B + "\n");
         }
         System.out.println(sb);
     }
